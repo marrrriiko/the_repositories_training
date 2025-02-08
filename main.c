c85ac33fc43c78beb0a385692e7a266c6aa13938
Коробкова Мария <makorobkova2007@gmail.com>
2025-02-08 09:56:45 +0300

    Revert "Promt user for last name as well."
    This reverts commit cc179e2236ba935fabdc28787f37949895329bac.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* main.c */ 
 #include <stdio.h> 
 #include <string.h> // Не забудьте подключить библиотеку для strlen
@@ -13,4 +14,19 @@ int main(int argc, char **argv)
     printf("Hello, %s %s!\n", first, last); 
 
     return 0; 
+=======
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    char name[255];
+
+    printf("Enter your name: ");
+    fgets(name, 255, stdin);
+    printf("length = %d\n", strlen(name)); /* debug line */
+    name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+
+    printf("Hello %s!\n", name);
+    return 0;
+>>>>>>> parent of cc179e2 (Promt user for last name as well.)
 }
